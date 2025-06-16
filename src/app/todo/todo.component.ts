6bda861896027f45ea3a7970dc1b3273c8ce699c
PramisGrg <gurungpramis361@gmail.com>
2025-06-16 21:25:54 +0545

    feat: error fixes

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-todo',
@@ -10,13 +11,17 @@ import { Component, signal } from '@angular/core';
 })
 export class Todo {
   newTodo = signal('');
-  todos = signal<string[]>([]);
+  todos = signal<{ id: string; todo: string }[]>([]);
 
   addTodo() {
     const value = this.newTodo().trim();
     if (value) {
-      this.todos.update((todos) => [...todos, value]);
+      this.todos.update((todos) => [...todos, { id: uuidv4(), todo: value }]);
       this.newTodo.set('');
     }
+  }
+
+  deleteTodo(id: string) {
+    this.todos.update((todos) => todos.filter((todo) => todo.id !== id));
   }
 }
